05ae1ac4c1c3aaf51834ddc5e3455f82c092cc53
Tendongze Godson <tendongze95@gmail.com>
2020-04-05 12:11:13 +0100

    made the following changes
    - made footer sticky
    - made the logo to be a link
    - added some css to make the web page look exactly like the reference
    - did all that the TSE recommended

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("header");
+var footer = document.getElementById("sticky-footer");
 
 var sticky = header.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    footer.classList.add("sticky-base");
   } else {
     header.classList.remove("sticky");
+     footer.classList.remove("sticky-base");
   }
 }
